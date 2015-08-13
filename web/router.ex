d933b8ccc561bceee1f86a14924e94c9b07c0d8c
Ulrik Augustsson <ulrikaugustsson@gmail.com>
2015-08-13 11:33:25 +0200

    add json as accepted content type

--- web/router.ex
+++ web/router.ex
@@ -2,7 +2,7 @@ defmodule Chat.Router do
   use Phoenix.Router
 
   pipeline :browser do
-    plug :accepts, ["html"]
+    plug :accepts, ["html", "json"]
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
